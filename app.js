cebcf0931b6b38fb14d7d047da27023b21a985a6
Mandinga31 <gonzalo.rozas@gmail.com>
2022-01-21 21:52:44 -0300

    Prueba Proyecto HTML

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const path = require('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use ( express.static(publicPath));
+
+app.listen( 3000, () => {
+        console.log("Servidor corriendo en el puero 3000");
+});
+
+app.get('/', (req, res) => {    
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
